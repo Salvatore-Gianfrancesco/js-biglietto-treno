ddd95a543607f0c25c28135d5d2061378d99b2d3
Salvatore-Gianfrancesco <salvatore.gianfrancesco@yahoo.it>
2022-10-06 14:27:34 +0200

    javascript exercise

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,22 @@
 /* JavaScript file */
 
+const distance = Number(prompt("Inserisci il numero di chilometri da percorrere"));
+const age = Number(prompt("Inserisci l'età"));
+// console.lo50g(distance, age);
+
+const fullPrice = distance * 0.21;
+// console.log(fullPrice);
+
+let discount = 0;
+if (age < 18) {
+    discount = fullPrice * 20 / 100;
+} else if (age > 65) {50
+    discount = fullPrice * 40 / 100;
+}
+// console.log(discount);
+
+let ticketPrice = fullPrice - discount;
+ticketPrice = Number(ticketPrice.toFixed(2));
+// console.log(ticketPrice);
+
+console.log(`Il biglietto costa ${ticketPrice}€`);
