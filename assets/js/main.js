63b8bf6a41ac1401b5016d4bd1f94a9e4df42bab
Salvatore-Gianfrancesco <salvatore.gianfrancesco@yahoo.it>
2022-10-06 19:25:20 +0200

    added initial condition on input

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,27 +2,45 @@
 
 const distance = Number(prompt("Inserisci il numero di chilometri da percorrere"));
 const age = Number(prompt("Inserisci l'età"));
-// console.lo50g(distance, age);
+// console.log(distance, age);
 
-const fullPrice = distance * 0.21;
-// console.log(fullPrice);
+if ((distance > 0) && (age > 0 && age < 121)) {
+    const fullPrice = distance * 0.21;
+    // console.log(fullPrice);
 
-let discount = 0;
-if (age < 18) {
-    discount = fullPrice * 20 / 100;
-} else if (age > 65) {50
-    discount = fullPrice * 40 / 100;
+    let discount = 0;
+    if (age < 18) {
+        discount = fullPrice * 20 / 100;
+    } else if (age > 65) {
+        50
+        discount = fullPrice * 40 / 100;
+    }
+    // console.log(discount);
+
+    let ticketPrice = fullPrice - discount;
+    ticketPrice = Number(ticketPrice.toFixed(2));
+    // console.log(ticketPrice);
+
+    console.log(`Il biglietto costa ${ticketPrice}€`);
+
+    document.getElementById("site_age").innerHTML = age;
+    document.getElementById("site_distance").innerHTML = distance;
+    document.getElementById("site_discount").innerHTML = discount.toFixed(2);
+    document.getElementById("site_price").innerHTML = ticketPrice;
+} else {
+    alert("Hai inserito distanza o età in formato errato!! Ricarica e riprova");
+
+    document.getElementById("site_age").innerHTML = "???";
+    document.getElementById("site_distance").innerHTML = "???";
+    document.getElementById("site_discount").innerHTML = "???";
+    document.getElementById("site_price").innerHTML = "???";
 }
-// console.log(discount);
-
-let ticketPrice = fullPrice - discount;
-ticketPrice = Number(ticketPrice.toFixed(2));
-// console.log(ticketPrice);
-
-console.log(`Il biglietto costa ${ticketPrice}€`);
 
 
-document.getElementById("site_age").innerHTML = age;
-document.getElementById("site_distance").innerHTML = distance;
-document.getElementById("site_discount").innerHTML = discount.toFixed(2);
-document.getElementById("site_price").innerHTML = ticketPrice;
+
+
+
+
+
+
+
